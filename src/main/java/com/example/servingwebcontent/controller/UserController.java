3b20da1f2296c5da4ecdf95f6a375918ac8dc791
Maistrenko Nikita <slipknoshka777@gmail.com>
2021-04-15 00:44:40 +0300

    added UI with bootstrap

---
+++ src/main/java/com/example/servingwebcontent/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.example.servingwebcontent.controller;
+
+import com.example.servingwebcontent.domain.Role;
+import com.example.servingwebcontent.domain.User;
+import com.example.servingwebcontent.repos.UserRepo;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
+public class UserController {
+    private final UserRepo userRepo;
+
+    public UserController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepo.findAll());
+
+        return "userList";
+    }
+
+    //current user
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    //method of saving changes to user data
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user
+    ) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+
+        return "redirect/user";
+    }
+}
---
+++ src/main/resources/templates/parts/security.ftl
@@ -0,0 +1,17 @@
+<!-- removing user list for not admin -->
+<#assign
+known = Session.SPRING_SECURITY_CONTEXT??
+>
+
+<#if known>
+    <#assign
+    user = Session.SPRING_SECURITY_CONTEXT.authentication.principal
+    name = user.getUsername()
+    isAdmin = user.isAdmin()
+    >
+<#else>
+    <#assign
+    name = "unknown"
+    isAdmin = false
+    >
+</#if>
